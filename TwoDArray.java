0ee694fca3f858f674a1267adc66b113fed77bf9
asimbinahammed <asim242370@gmail.com>
2021-08-19 15:25:30 +0530

    additional problems

---
+++ TwoDArray.java
@@ -0,0 +1,42 @@
+package bridgeLabz;
+
+import java.util.Scanner;
+
+public class TwoDArray {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	    System.out.print("Enter row size: ");
+	    int m = sc.nextInt();
+	    
+	    Scanner sd = new Scanner(System.in);
+	    System.out.print("Enter column size: ");
+	    int n = sd.nextInt();
+	    
+		int Array[][] = new int[m][n];
+
+	    for (int i = 0; i < m; i++) {
+	    	for(int j=0; j<n; j++) {
+	    		Scanner ij = new Scanner(System.in);
+	    	    System.out.print("Enter element ");
+	    	   int mn =ij.nextInt();
+	    	   Array[i][j]=mn;
+	    	}
+	    	 
+	    	}
+	
+	    for (int i = 0; i < m; i++) {
+	    	for(int j=0; j<n; j++) {
+	    		
+	    		System.out.println( Array[i][j]);  
+
+	    	}
+	    	System.out.println();
+	    	
+	    	}
+	   
+	    	}
+	    
+	    
+	}
+
